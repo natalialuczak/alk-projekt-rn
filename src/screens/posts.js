70c0480ecaf0064d5b7ac41d2b0733f308fd90db
natalialuczak <natluc121@gmail.com>
2023-05-26 00:21:32 +0200

    sg gotowy

--- src/screens/posts.js
+++ src/screens/posts.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList, Image, Dimensions } from "react-native";
+import { View, Text, StyleSheet, FlatList, Image, Dimensions, TouchableOpacity } from "react-native";
 import { useQuery } from "@tanstack/react-query";
+import { Feather } from "@expo/vector-icons";
 
 const API_URL = "https://rickandmortyapi.com/api/character";
 
@@ -11,6 +12,46 @@ const fetchPosts = async () => {
 };
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
+
+const PostFooter = () => (
+  <View style={styles.footerContainer}>
+    <View style={styles.iconsContainer}>
+      <TouchableOpacity style={styles.iconContainer}>
+        <Feather name="heart" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.iconContainer}>
+        <Feather name="message-circle" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.iconContainer}>
+        <Feather style={styles.icon} name="send" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+    <View style={styles.likesCommentsContainer}>
+      <Text style={styles.likesText}>120 likes</Text>
+      {/* <Text style={styles.commentsText}>25 comments</Text> */}
+    </View>
+  </View>
+);
+
+const PostDescriptions = () => (
+  <View style={styles.commentsContainer}>
+    <Text style={styles.commentText}>
+      <Text style={styles.commentUsername}></Text>Lorem Ipsum is
+      simply dummy text of the printing and typesetting industry. ❤️{" "}
+    </Text>
+  </View>
+);
+
+const PostComments = () => (
+  <View style={styles.commentsContainer}>
+    <Text style={styles.commentText}>
+      <Text style={styles.commentviewall}>
+        Zobacz wszystkie komentarze: 100
+      </Text>
+    </Text>
+    <Text style={styles.commenttransalte}>Zobacz tłumaczenie</Text>
+  </View>
+);
 
 const Posts = () => {
   const { data, isLoading, isError } = useQuery(["posts"], fetchPosts);
@@ -33,18 +74,16 @@ const Posts = () => {
 
   const renderItem = ({ item }) => (
     <View style={styles.postContainer}>
-      <View style={styles.userInfoContainer}>
+      <View style={styles.postHeader}>
         <Image style={styles.avatar} source={{ uri: item.image }} />
         <Text style={styles.username}>{item.name}</Text>
       </View>
-      <Image style={styles.postImage} source={{ uri: item.image }} />
-      <View style={styles.postContent}>
-        <Text style={styles.likes}>{item.likes} likes</Text>
-        <Text style={styles.description}>{item.description}</Text>
-        <Text style={styles.comments}>
-          {item.comments ? item.comments.length : 0} comments
-        </Text>
+      <View style={styles.postImageContainer}>
+        <Image style={styles.postImage} source={{ uri: item.image }} />
       </View>
+      <PostFooter />
+      <PostDescriptions />
+      <PostComments/>
     </View>
   );
 
@@ -65,47 +104,79 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   flatListContainer: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingBottom: 20,
   },
   postContainer: {
     marginBottom: 20,
-    borderWidth: 1,
-    borderColor: "#ddd",
-    borderRadius: 10,
-    overflow: "hidden",
   },
-  userInfoContainer: {
+  postHeader: {
     flexDirection: "row",
     alignItems: "center",
     padding: 10,
-    backgroundColor: "#f9f9f9",
   },
   avatar: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
   username: {
+    marginLeft: 10,
     fontWeight: "bold",
   },
+  postImageContainer: {
+    alignItems: "center",
+  },
   postImage: {
-    width: SCREEN_WIDTH - 40,
-    height: SCREEN_WIDTH - 40,
-    resizeMode: "cover",
+    width: SCREEN_WIDTH * 0.95,
+    height: SCREEN_WIDTH * 0.95,
   },
-  postContent: {
-    padding: 10,
+  footerContainer: {
+    marginTop: 10,
+    paddingHorizontal: 10,
   },
-  likes: {
-    marginBottom: 5,
+  iconsContainer: {
+    flexDirection: "row",
+    marginBottom: 10,
   },
-  description: {
-    marginBottom: 5,
+  iconContainer: {
+    marginRight: 12,
   },
-  comments: {
+  likesCommentsContainer: {
+    flexDirection: "row",
+  },
+  likesText: {
+    marginRight: 10,
+    fontWeight: "bold",
+  },
+  commentsText: {
     color: "gray",
+  },
+  commentsContainer: {
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  commentText: {
+    marginBottom: 5,
+  },
+  commentUsername: {
+    fontWeight: "bold",
+  },
+  commentsContainer: {
+    marginTop: 4,
+    paddingHorizontal: 10,
+  },
+  commentUsername: {
+    fontWeight: "bold",
+    marginRight: 4,
+  },
+  commenttransalte: {
+    fontWeight: "700",
+    fontSize: 10,
+    marginTop: 4,
+  },
+  commentviewall: {
+    fontWeight: "500",
+    fontSize: 12,
   },
 });
 
